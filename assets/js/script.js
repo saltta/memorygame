4b50c327ddd0229526ad6995ed7b406ad8b7c015
saltta <trottsal@gmail.com>
2021-10-21 07:51:28 +0000

    started game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,22 +14,34 @@ class MemoryGame {
     }
     startGame() {
         this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+    }
+    flipCard(card) {
+
+    }
+    canFlipCard(card) {
+        return true;
+        //return !this.busy && !this.matchedCards.includes(card)&& card !== this.cardToCheck;
     }
 }
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MemoryGame(60, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
-            // game.startGame();
+            game.startGame();
         });
     });
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            //game.flipCard(card);
+            game.flipCard(card);
         });
     });
 }
