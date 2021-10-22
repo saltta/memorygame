91f0717c7155e27877884a92668bffaa490e68a5
saltta <trottsal@gmail.com>
2021-10-22 02:42:58 +0000

    finished documenting script and readded DOM loaded event listener to start game

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
  * Creates and controls data for the game 
  */
 class MemoryGame {
+
     /**
      * Sets the template for the game
      * @param {number} totalTime - Total time to play
@@ -14,6 +15,7 @@ class MemoryGame {
         this.timer = document.getElementById('time');
         this.ticker = document.getElementById('flips');
     }
+
     /**
      * Starts the game
      */
@@ -24,48 +26,63 @@ class MemoryGame {
         this.matchedCards = [];
         this.busy = true;
 
-        /**
-         * Delays the start of the game after you click for smoother experience
-         */
+        //Delays the start of the game after click for smoother experience
         setTimeout(() => {
             this.shuffleCards();
             this.countDown = this.startCountDown();
             this.busy = false;
         }, 500);
-        
+
+        //Resets the timer and flip count
         this.hideCards();
         this.timer.innerText = this.timeRemaining;
         this.ticker.innerText = this.totalClicks;
     }
 
+    /**
+     * Loops through the cards array and flips the cards back
+     */
     hideCards() {
         this.cardsArray.forEach(card => {
             card.classList.remove('visible');
         });
     }
 
+    /**
+     * Flips the clicked card and updates the flip count
+     * @param {object} card - The clicked card
+     */
     flipCard(card) {
-        if(this.canFlipCard(card)) {
+        if (this.canFlipCard(card)) {
             this.totalClicks++;
             this.ticker.innerText = this.totalClicks;
             card.classList.add('visible');
 
-            if ( this.cardToCheck)
+            if (this.cardToCheck)
                 this.checkForCardMatch(card);
             else
                 this.cardToCheck = card;
         }
     }
 
+    /**
+     * Checks if the clicked pair of cards are the same
+     * @param {object} card - The clicked card
+     */
     checkForCardMatch(card) {
         if (this.getCardType(card) === this.getCardType(this.cardToCheck))
             this.cardMatch(card, this.cardToCheck);
-        else   
+        else
             this.cardMismatch(card, this.cardToCheck);
-        
-            this.cardToCheck = null;
+
+        this.cardToCheck = null;
     }
 
+    /**
+     * Displays flipped pair if they match and a victory message if all cards have been matched
+     * @param {object} card1 - The first clicked card
+     * @param {object} card2 - The second clicked card
+     */
     cardMatch(card1, card2) {
         this.matchedCards.push(card1);
         this.matchedCards.push(card2);
@@ -73,52 +90,81 @@ class MemoryGame {
             this.endGame('victory');
     }
 
+    /**
+     * Flips the pair of mismatched cards back over after a small delay
+     * @param {object} card1 - The first clicked card
+     * @param {object} card2 - The second clicked card
+     */
     cardMismatch(card1, card2) {
         this.busy = true;
         setTimeout(() => {
             card1.classList.remove('visible');
             card2.classList.remove('visible');
             this.busy = false;
-        }, 800);
+        }, 650);
     }
 
+    /**
+     * Gets a value to identify the clicked card
+     * @param {object} card - The card clicked
+     * @returns {string} - the value of a class
+     */
     getCardType(card) {
         return card.getElementsByClassName('fas')[1].classList[1];
     }
 
+    /**
+     * Counts the time down one second at a time, ends the game if time runs out
+     */
     startCountDown() {
         return setInterval(() => {
             this.timeRemaining--;
             this.timer.innerText = this.timeRemaining;
-            if(this.timeRemaining === 0)
-                this.endGame('game-over');            
+            if (this.timeRemaining === 0)
+                this.endGame('game-over');
         }, 1000);
     }
 
+    /**
+     * Stops the countdown and displays win/lose message
+     * @param {string} outcome - Id's the overlay to become visible
+     */
     endGame(outcome) {
         clearInterval(this.countDown);
         document.getElementById(outcome).classList.add('visible');
     }
 
+    /**
+     * Fisher-Yates shuffle algorithm, shuffles the cards 
+     */
     shuffleCards() {
-        for(let i = this.cardsArray.length - 1; i > 0; i--) {
-            let randIndex = Math.floor(Math.random() * (i+1));
+        for (let i = this.cardsArray.length - 1; i > 0; i--) {
+            let randIndex = Math.floor(Math.random() * (i + 1));
             this.cardsArray[randIndex].style.order = i;
             this.cardsArray[i].style.order = randIndex;
         }
     }
 
+    /**
+     * Determines if the card can be flipped
+     * @param {object} card - The card being clicked
+     * @returns {boolean} - If true the card can be flipped
+     */
     canFlipCard(card) {
         return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
     }
 
 }
 
+/**
+ * Creates arrays out of HTML collections and loops through them and initiates the game
+ */
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
     let game = new MemoryGame(45, cards);
 
+    //Loops and adds click events to the overlay messages
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
@@ -126,12 +172,20 @@ function ready() {
         });
     });
 
+    //Loops and adds click events to the cards
     cards.forEach(card => {
         card.addEventListener('click', () => {
             game.flipCard(card);
         });
     });
-    
+
 }
 
-ready();
+/**
+ * Makes sure the DOM has been loaded before starting the game
+ */
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
